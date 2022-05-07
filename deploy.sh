35d69fbee6cf6e9db50521672c8a0b08629ba6c1
Uladzimir Luhautsou <lugovczov@gmail.com>
2022-05-07 15:49:30 +0400

    fix(build): update deploy settings

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,9 @@ npm run build
 cd dist
 
 git init
-git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:lugovczov/days-in-quarter.git main:gh-pages
+git push -f git@github.com:lugovczov/days-in-quarter.git master:gh-pages
+
+cd -
